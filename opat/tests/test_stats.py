ec04e39a53eda6c5cb95cd0ce8a89cb1a234d9ae
Shawn Lin <xiaoliang.lin@columbia.edu>
2018-07-03 23:15:36 -0400

    Add more tests

--- opat/tests/test_stats.py
+++ opat/tests/test_stats.py
@@ -1,5 +1,7 @@
 from opat.stats import (cum_return,
-                        vami,)
+                        vami,
+                        period_return,
+                        annualized_return)
 
 from opat.io import read_ts_csv
 
@@ -11,6 +13,10 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(),
 returns_data = read_ts_csv(__location__ + '/test_data/fund_return.csv')
 
 print(returns_data.head())
-print(type(returns_data))
-print(cum_return(returns_data))
-print(vami(returns_data))
+print(cum_return(returns_data).head())
+print(vami(returns_data).head())
+print(period_return(returns_data, "month").head())
+print(period_return(returns_data, "week").head())
+print(period_return(returns_data, "year").head())
+print(period_return(returns_data, "quarter").head())
+print(annualized_return(returns_data))
