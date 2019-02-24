f7461815b0a50e943928bb11aaecd40ae2c5c305
Shawn Lin <xiaoliang.lin@columbia.edu>
2019-02-24 15:32:21 -0500

    Update test due to removed module.

--- opat/tests/test_stats.py
+++ opat/tests/test_stats.py
@@ -6,17 +6,11 @@ from opat.stats import (cum_return,
                         annualized_return,
                         annualized_std,)
 
-from opat.plot import (ts_to_hc_series,
-                       highstock_line,)
-
-from opat.portfolio import (create_holdings,
-                            get_holdings_equity_price)
+from opat.portfolio import (create_holdings)
 
 from opat.io import read_ts_csv
 
-
-__location__ = os.path.realpath(os.path.join(
-    os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 returns_data = read_ts_csv(__location__ + '/test_data/fund_return.csv')
 trade_data = read_ts_csv(__location__ + '/test_data/trade_log.csv')
@@ -31,13 +25,3 @@ print(period_return(returns_data, "year").head())
 print(period_return(returns_data, "quarter").head())
 print(annualized_return(returns_data))
 print(annualized_std(returns_data))
-
-hc_series = ts_to_hc_series(vami(returns_data))
-html_string = highstock_line(hc_series, title="VAMI")
-
-with open(__location__ + '/examples/vami.html', 'w') as html_file:
-    html_file.write(html_string)
-
-
-price_data = get_holdings_equity_price(create_holdings(trade_data))
-price_data.to_csv(__location__ + '/test_data/price_data.csv')
